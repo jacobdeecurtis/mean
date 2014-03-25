bd9775ec7870d7c72e999205df5a4b0a6fabe067
Jacob Curtis <jacobdeecurtis@gmail.com>
2014-03-24 22:53:49 -0600

    trying to add compareGroup route

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,5 +5,12 @@ module.exports = function(app) {
     // Home route
     var index = require('../controllers/index');
     app.get('/', index.render);
+    app.get('/compareGroups', compareGroups.all);
+    app.post('/compareGroups',  compareGroups.create);
 
 };
+
+
+
+
+
