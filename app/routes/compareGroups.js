a1a567efbb72943dcd8982186c93c60121affd4d
Jacob Curtis <jacobdeecurtis@gmail.com>
2014-03-24 18:31:02 -0600

    adding app routes  for Decision Machine part

---
+++ app/routes/compareGroups.js
@@ -0,0 +1,26 @@
+'use strict';
+
+// Articles routes use articles controller
+var compareGroups = require('../controllers/compareGroups');
+var authorization = require('./middlewares/authorization');
+
+// Article authorization helpers
+var hasAuthorization = function(req, res, next) {
+	if (req.compareGroup.user.id !== req.user.id) {
+        return res.send(401, 'User is not authorized');
+    }
+    next();
+};
+
+module.exports = function(app) {
+
+    app.get('/compareGroups', compareGroups.all);
+    app.post('/compareGroups', authorization.requiresLogin, compareGroups.create);
+    app.get('/compareGroups/:compareGroupId', compareGroups.show);
+    app.put('/compareGroups/:compareGroupId', authorization.requiresLogin, hasAuthorization, compareGroups.update);
+    app.del('/compareGroups/:compareGroupId', authorization.requiresLogin, hasAuthorization, compareGroups.destroy);
+
+    // Finish with setting up the articleId param
+    app.param('compareGroupId', compareGroups.compareGroup);
+
+};
