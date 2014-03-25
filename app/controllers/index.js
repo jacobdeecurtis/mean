39437765f55ba33890eb453ddb8da0af79316ede
Jacob Curtis <jacobdeecurtis@gmail.com>
2014-03-25 16:53:55 -0600

    test

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -19,93 +19,47 @@ exports.render = function(req, res) {
 /**
  * Find article by id
  */
-exports.compareGroup = function(req, res, next, id) {
-    CompareGroup.load(id, function(err, compareGroup) {
-        if (err) return next(err);
-        if (!compareGroup) return next(new Error('Failed to load compareGroup ' + id));
-        req.compareGroup = compareGroup;
-        next();
-    });
-};
+// exports.compareGroup = function(req, res, next, id) {
+//     CompareGroup.load(id, function(err, compareGroup) {
+//         if (err) return next(err);
+//         if (!compareGroup) return next(new Error('Failed to load compareGroup ' + id));
+//         req.compareGroup = compareGroup;
+//         next();
+//     });
+// };
 
 /**
  * Create an article
  */
-exports.create = function(req, res) {
-    var compareGroup = new CompareGroup(req.body);
-    compareGroup.user = req.user;
+// exports.create = function(req, res) {
+//     var compareGroup = new CompareGroup(req.body);
+//     compareGroup.user = req.user;
 
-    compareGroup.save(function(err) {
-        if (err) {
-            return res.send('users/signup', {
-                errors: err.errors,
-                compareGroup: compareGroup
-            });
-        } else {
-            res.jsonp(compareGroup);
-        }
-    });
-};
-
-/**
- * Update an article
- */
-// exports.update = function(req, res) {
-//     var article = req.article;
-
-//     article = _.extend(article, req.body);
-
-//     article.save(function(err) {
+//     compareGroup.save(function(err) {
 //         if (err) {
 //             return res.send('users/signup', {
 //                 errors: err.errors,
-//                 article: article
+//                 compareGroup: compareGroup
 //             });
 //         } else {
-//             res.jsonp(article);
+//             res.jsonp(compareGroup);
 //         }
 //     });
 // };
-
-/**
- * Delete an article
- */
-// exports.destroy = function(req, res) {
-//     var article = req.article;
-
-//     article.remove(function(err) {
-//         if (err) {
-//             return res.send('users/signup', {
-//                 errors: err.errors,
-//                 article: article
-//             });
-//         } else {
-//             res.jsonp(article);
-//         }
-//     });
-// };
-
-/**
- * Show an article
- */
-// exports.show = function(req, res) {
-//     res.jsonp(req.article);
-// };
-
 
 
 
 /**
  * List of Articles
  */
-exports.all = function(req, res) {
-    CompareGroup.find().sort('-created').populate('user', 'name username').exec(function(err, compareGroups) {
-        if (err) {
-            res.render('error', {
-                status: 500
-            });
-        } else {
-            res.jsonp(compareGroups);
-        }
-    });
-};
+// exports.all = function(req, res) {
+//     CompareGroup.find().sort('-created').populate('user', 'name username').exec(function(err, compareGroups) {
+//         if (err) {
+//             res.render('error', {
+//                 status: 500
+//             });
+//         } else {
+//             res.jsonp(compareGroups);
+//         }
+//     });
+// };
--- app/routes/index.js
+++ app/routes/index.js
@@ -5,8 +5,8 @@ module.exports = function(app) {
     // Home route
     var index = require('../controllers/index');
     app.get('/index',  index.render);
-    app.post('/index',  compareGroups.create);
-    app.get('/index', compareGroups.all);
+    // app.post('/index',  compareGroups.create);
+    // app.get('/index', compareGroups.all);
     
 app.param('compareGroupId', compareGroups.compareGroup);
 };
