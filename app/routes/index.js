5fc622952bcc396d7baf6a1d299873a721196aa0
Jacob Curtis <jacobdeecurtis@gmail.com>
2014-03-24 23:31:05 -0600

    test

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,8 +5,8 @@ module.exports = function(app) {
     // Home route
     var index = require('../controllers/index');
     app.get('/', index.render);
-    app.get('/index', compareGroups.all);
-    app.post('/index',  compareGroups.create);
+    app.get('/compareGroups', compareGroup.all);
+    app.post('/compareGroups',  compareGroup.create);
 
 };
 
