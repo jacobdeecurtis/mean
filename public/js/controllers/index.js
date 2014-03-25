abb88f71c388eaeba99370aa18c4b7881d7f7048
Jacob Curtis <jacobdeecurtis@gmail.com>
2014-03-25 14:58:02 -0600

    Editing the public index controller

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -1,5 +1,82 @@
 'use strict';
 
-angular.module('mean.compareGroups').controller('IndexController', ['$scope', 'Global', function ($scope, Global) {
+angular.module('mean.system').controller('IndexController', ['$scope', 'Global', function ($scope, Global) {
     $scope.global = Global;
+
+
+
+
+    //this creates array allowing multiple attributes to be created using ng-click and ng-repeat on create.html
+    // $scope.attributes = new Array();
+    // var blankAttribute = {attributeName: null, score: null, uom: null};
+    //          $scope.attributesArray = [blankAttribute]; 
+  
+    // $scope.add_attribute = function() {
+    //     var newAttr = angular.copy(blankAttribute);
+    //     $scope.attributesArray.push(newAttr);
+    // };
+
+    $scope.create = function() {
+        var compareGroup = new CompareGroups({
+            //"this" is the same thing as scope
+            name: this.name,
+            picture: this.picture,
+            attributes: this.attributesArray,
+            score: this.attributesArray.score
+        });
+
+        compareGroup.$save(function(response) {
+            $location.path('index/' + response._id);
+        });
+
+        // this.name = '';
+        // this.image ='';
+        // this.attributes ='';
+        // this.scores ='';
+        // this.unitOfMeasurement ='';
+        
+    };
+
+ $scope.remove = function(article) {
+        if (article) {
+            article.$remove();
+
+            for (var i in $scope.articles) {
+                if ($scope.articles[i] === article) {
+                    $scope.articles.splice(i, 1);
+                }
+            }
+        }
+        else {
+            $scope.article.$remove();
+            $location.path('articles');
+        }
+    };
+
+    $scope.update = function() {
+        var article = $scope.article;
+        if (!article.updated) {
+            article.updated = [];
+        }
+        article.updated.push(new Date().getTime());
+
+        article.$update(function() {
+            $location.path('articles/' + article._id);
+        });
+    };
+
+    $scope.find = function() {
+        Articles.query(function(articles) {
+            $scope.articles = articles;
+        });
+    };
+
+    $scope.findOne = function() {
+        Articles.get({
+            articleId: $stateParams.articleId
+        }, function(article) {
+            $scope.article = article;
+        });
+    };
+    
 }]);
