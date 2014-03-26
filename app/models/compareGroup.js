0b1528d57567fb4386540ffcc1a24fc605bc3ba3
Jacob Curtis <jacobdeecurtis@gmail.com>
2014-03-25 18:22:55 -0600

    test

---
+++ app/models/compareGroup.js
@@ -0,0 +1,51 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+
+/**
+ * Article Schema
+ */
+var CompareGroupSchema = new Schema({
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    optionName: {
+        type: String,
+        default: '',
+        trim: true
+    },
+    optionAttributes: [{
+        attributeName: String,
+        optionImportance: Number,
+        optionScore: Number,
+        optionuom: String
+    }],
+    user: {
+        type: Schema.ObjectId,
+        ref: 'User'
+    }
+});
+
+/**
+ * Validations
+ */
+CompareGroupSchema.path('optionName').validate(function(optionName) {
+    return optionName.length;
+}, 'Title cannot be blank');
+
+/**
+ * Statics
+ */
+CompareGroupchema.statics.load = function(id, cb) {
+    this.findOne({
+        _id: id
+    }).populate('user', 'name username').exec(cb);
+};
+
+mongoose.model('CompareGroup', CompareGroupSchema);
