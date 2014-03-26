f94ff548ab3b7832ff779047890661c635bdbbab
Jacob Curtis <jacobdeecurtis@gmail.com>
2014-03-25 19:30:03 -0600

    test

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('mean.system').controller('IndexController', ['$scope', 'Global', function ($scope, Global) {
+angular.module('mean.system').controller('IndexController', ['$scope', 'Global', 'CompareGroups', function ($scope, Global) {
     $scope.global = Global;
 
 
--- public/js/services/compareGroups.js
+++ public/js/services/compareGroups.js
@@ -1,8 +1,8 @@
 'use strict';
 
-//Articles service used for articles REST endpoint
+//Articles service used for compareGroups REST endpoint
 angular.module('mean.compareGroups').factory('compareGroups', ['$resource', function($resource) {
-    return $resource('index/:compareGroupId', {
+    return $resource('compareGroups/:compareGroupId', {
         compareGroupId: '@_id'
     }, {
         update: {
