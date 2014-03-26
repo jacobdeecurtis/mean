81b8508565b529d23e69ca2ca7bbea0282525887
Jacob Curtis <jacobdeecurtis@gmail.com>
2014-03-25 19:50:48 -0600

    test

--- public/js/services/compareGroups.js
+++ public/js/services/compareGroups.js
@@ -1,6 +1,6 @@
 'use strict';
 
-//Articles service used for compareGroups REST endpoint
+//Articles service used for articles REST endpoint
 angular.module('mean.compareGroups').factory('compareGroups', ['$resource', function($resource) {
     return $resource('compareGroups/:compareGroupId', {
         compareGroupId: '@_id'
