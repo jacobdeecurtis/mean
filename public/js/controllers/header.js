e6a4aa87cd2a8edce320b4198eee4c0ade54e7e3
Jacob Curtis <jacobdeecurtis@gmail.com>
2014-03-27 10:50:54 -0600

    test

--- public/js/controllers/header.js
+++ public/js/controllers/header.js
@@ -3,10 +3,12 @@
 angular.module('mean.system').controller('HeaderController', ['$scope', 'Global', function ($scope, Global) {
     $scope.global = Global;
 
-    $scope.menu = [{
-        'title': 'Articles',
-        'link': 'articles'
-    }, {
+    $scope.menu = [
+    // {
+    //     'title': 'Articles',
+    //     'link': 'articles'
+    // }, 
+    {
         'title': 'Enter Data',
         'link': 'articles/create'
     }, {
