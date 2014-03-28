fbb9883f9d01e12b85bf96c86abe37b14f8e377d
Jacob Curtis <jacobdeecurtis@gmail.com>
2014-03-27 22:10:55 -0600

    test

--- public/js/services/compareGroups.js
+++ public/js/services/compareGroups.js
@@ -2,11 +2,11 @@
 
 //Articles service used for articles REST endpoint
 angular.module('mean.compareGroups').factory('CompareGroups', ['$resource', function($resource) {
-    return $resource('!#/compareGroups', {
+    return $resource('compareGroups/:compareGroupId', {
         compareGroupId: '@_id'
     }, {
-        // update: {
-        //     // method: 'PUT'
-        // }
+        update: {
+            method: 'PUT'
+        }
     });
 }]);
