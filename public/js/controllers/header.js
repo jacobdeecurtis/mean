92b62fc7efe4102bb79f510af7b76d4bd22221f8
Jacob Curtis <jacobdeecurtis@gmail.com>
2014-03-31 16:48:38 -0600

    test

--- public/js/controllers/header.js
+++ public/js/controllers/header.js
@@ -4,10 +4,10 @@ angular.module('mean.system').controller('HeaderController', ['$scope', 'Global'
     $scope.global = Global;
 
     $scope.menu = [
-    {
-        'title': 'Articles',
-        'link': 'articles'
-    }, 
+    // {
+    //     'title': 'Articles',
+    //     'link': 'articles'
+    // }, 
     {
         'title': 'Decision Machine',
         'link': '/!#/compareGroups'
